f7ff288bf18c43d9ccd1ea3f237a9dd34e16fd78
Amanullah Aman <amancse.aiub@gmail.com>
2023-09-03 13:53:40 +0600

    Event search form

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,12 @@
 import EventList from "@/components/events/event-list";
+import EventSearch from "@/components/events/event-search";
 import { getAllEvents } from "@/dummy-data";
 
 const AllEventsPage = () => {
   const events = getAllEvents();
   return (
     <div>
+      <EventSearch />
       <EventList items={events} />
     </div>
   );
