3dc918c1215c7cd49b45d52b798fdb74d8b923eb
Amanullah Aman <amancse.aiub@gmail.com>
2023-09-03 14:20:19 +0600

    error show

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,4 +1,60 @@
+import EventList from "@/components/events/event-list";
+import ResultsTitle from "@/components/results-title/results-title";
+import Button from "@/components/ui/button";
+import ErrorAlert from "@/components/ui/error-alert/error-alert";
+import { getFilteredEvents } from "@/dummy-data";
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+
 const EventsSlug = () => {
-  return <h1>This is EventsSlugPage</h1>;
+  const router = useRouter();
+  const filterData = router.query.slug;
+  console.log(filterData);
+
+  if (!filterData) {
+    return (
+      <ErrorAlert>
+        <p className="center">Loading...</p>;
+      </ErrorAlert>
+    );
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (isNaN(numMonth) || isNaN(numYear) || numMonth < 1 || numMonth > 12) {
+    return (
+      <ErrorAlert>
+        <p className="center">Invalid filter</p>;
+      </ErrorAlert>
+    );
+  }
+
+  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>Sorry! no data found</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const date = new Date(numYear, numMonth - 1);
+
+  return (
+    <Fragment>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </Fragment>
+  );
 };
 export default EventsSlug;
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,6 +1,8 @@
 import EventContent from "@/components/event-detail/event-content";
 import EventLogistics from "@/components/event-detail/event-logistics";
 import EventSummary from "@/components/event-detail/event-summary";
+import Button from "@/components/ui/button";
+import ErrorAlert from "@/components/ui/error-alert/error-alert";
 import { getEventById } from "@/dummy-data";
 import { useRouter } from "next/router";
 import { Fragment } from "react";
@@ -11,7 +13,16 @@ const EventDetailPage = () => {
 
   const event = getEventById(eventId);
   if (!event) {
-    return <p>No event found</p>;
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>No event found</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
   }
 
   return (
