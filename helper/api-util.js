aae4398600c287568e40e1b811f820b0793153f3
Amanullah Aman <amancse.aiub@gmail.com>
2023-09-03 23:16:00 +0600

    server side props

--- helper/api-util.js
+++ helper/api-util.js
@@ -23,3 +23,18 @@ export async function getEventByIdFireBase(id) {
   const allEvents = await getAllEvents();
   return allEvents.find((event) => event.id === id);
 }
+
+export async function getFilteredEventsFirebase(dateFilter) {
+  const { year, month } = dateFilter;
+
+  const allEvents = await getAllEvents();
+
+  let filteredEvents = allEvents.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+    );
+  });
+
+  return filteredEvents;
+}
--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -3,29 +3,30 @@ import ResultsTitle from "@/components/results-title/results-title";
 import Button from "@/components/ui/button";
 import ErrorAlert from "@/components/ui/error-alert/error-alert";
 import { getFilteredEvents } from "@/dummy-data";
+import { getFilteredEventsFirebase } from "@/helper/api-util";
 import { useRouter } from "next/router";
 import { Fragment } from "react";
 
-const EventsSlug = () => {
+const EventsSlug = (props) => {
   const router = useRouter();
-  const filterData = router.query.slug;
-  console.log(filterData);
+  // const filterData = router.query.slug;
+  // console.log(filterData);
 
-  if (!filterData) {
-    return (
-      <ErrorAlert>
-        <p className="center">Loading...</p>;
-      </ErrorAlert>
-    );
-  }
+  // if (!filterData) {
+  //   return (
+  //     <ErrorAlert>
+  //       <p className="center">Loading...</p>;
+  //     </ErrorAlert>
+  //   );
+  // }
 
-  const filteredYear = filterData[0];
-  const filteredMonth = filterData[1];
+  // const filteredYear = filterData[0];
+  // const filteredMonth = filterData[1];
 
-  const numYear = +filteredYear;
-  const numMonth = +filteredMonth;
+  // const numYear = +filteredYear;
+  // const numMonth = +filteredMonth;
 
-  if (isNaN(numMonth) || isNaN(numYear) || numMonth < 1 || numMonth > 12) {
+  if (props.hasError) {
     return (
       <ErrorAlert>
         <p className="center">Invalid filter</p>
@@ -33,7 +34,7 @@ const EventsSlug = () => {
     );
   }
 
-  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
+  const filteredEvents = props.events;
 
   if (!filteredEvents || filteredEvents.length === 0) {
     return (
@@ -48,7 +49,7 @@ const EventsSlug = () => {
     );
   }
 
-  const date = new Date(numYear, numMonth - 1);
+  const date = new Date(props.date.year, props.date.month - 1);
 
   return (
     <Fragment>
@@ -57,4 +58,30 @@ const EventsSlug = () => {
     </Fragment>
   );
 };
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+
+  const filterData = params.slug;
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (isNaN(numMonth) || isNaN(numYear) || numMonth < 1 || numMonth > 12) {
+    return { props: { hasError: true } /*notFound: true*/ };
+  }
+
+  const filteredEvents = await getFilteredEventsFirebase({
+    year: numYear,
+    month: numMonth,
+  });
+
+  return {
+    props: { events: filteredEvents, date: { year: numYear, month: numMonth } },
+  };
+}
+
 export default EventsSlug;
